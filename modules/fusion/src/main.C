15a7f8dd1cbfa6b45e3cd9d85d8341b88c15bf5d
Casey Icenhour <casey.icenhour@inl.gov>
2025-03-13 16:24:21 -0600

    Fixup deprecated or modified code
    - In FVFunctionalNormalDirichletBC, add missing state parameter
    - Update to newer template for modules/fusion/src/main.C

--- modules/fusion/src/main.C
+++ modules/fusion/src/main.C
@@ -8,29 +8,11 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "FusionTestApp.h"
-#include "MooseInit.h"
-#include "Moose.h"
-#include "MooseApp.h"
-#include "AppFactory.h"
-
-// Create a performance log
-PerfLog Moose::perf_log("FusionApp");
+#include "MooseMain.h"
 
 // Begin the main program.
 int
 main(int argc, char * argv[])
 {
-  // Initialize MPI, solvers and MOOSE
-  MooseInit init(argc, argv);
-
-  // Register this application's MooseApp and any it depends on
-  FusionTestApp::registerApps();
-
-  // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("FusionTestApp", argc, argv);
-
-  // Execute the application
-  app->run();
-
-  return 0;
+  return Moose::main<FusionTestApp>(argc, argv);
 }
